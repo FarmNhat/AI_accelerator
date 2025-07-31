8636a565be2cb98857d7ebc11e9a6161e499e869
FarmNhat <nhatpham10925@gmail.com>
2025-07-31 23:44:48 +0700

    psum is now fifo

--- PE/parsum_spad.v
+++ PE/parsum_spad.v
@@ -1,6 +1,7 @@
 module parsum_spad (
     input  wire        clk,
     input  wire        rd, wr,      
+    input  wire        rst,       // Synchronous reset
     //input  wire [5:0]  addr_in,        
     input  wire [5:0]  addr_out,       
     input  wire [7:0]  data_in,   // 8-bit input data
@@ -8,16 +9,24 @@ module parsum_spad (
 );
 
     reg [7:0] mem [64:0];
-    reg count;
+    reg [5:0] count;
+    reg [5:0] head, tail;
 
     always @(posedge clk) begin
-        if(rst)
+        if(rst) begin
             count <= 0;
-            data_out <= 8'b00000000;                     // Reset count on reset signal
-        if (wr)
-            mem[count] <= data_in;         // Reset output to zero
-            count <= count + 1;             // Increment count on write
-        if (rd)
-            data_out <= mem[addr_out];       // Load new data when enabled
+            head <= 0;
+            tail <= 0;                
+            data_out <= 8'b00000000;
+        end                     // Reset count on reset signal
+        if (wr) begin
+            mem[tail] <= data_in;         // Reset output to zero
+            count <= count + 1;  
+            tail <= tail + 1;             // Increment tail pointer
+        end           // Increment count on write
+        if (rd) begin
+            data_out <= mem[head];  
+            head <= head + 1;             // Increment head pointer
+        end     // Load new data when enabled
     end
 endmodule
